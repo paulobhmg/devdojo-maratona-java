0bcdba099eb2cce828f225b9135f32ada5929aa4
Paulo Nogueira <paulobh.nogueira@gmail.com>
2022-03-16 13:36:02 -0700

    Utilização de operadores lógicos e relacionais.

---
+++ src/com/paulo/m01_basico/A018_OperadoresLogicosERelacionais.java
@@ -0,0 +1,25 @@
+package com.paulo.m01_basico;
+
+public class A018_OperadoresLogicosERelacionais {
+    public static void main(String[] args) {
+        // Operadores relacionais < > >= <= != !
+        double valorEmCC = 3000;
+        double valorEmCP = 2000;
+        double valorPS5 = 7000;
+        boolean credito = true;
+
+        boolean valorEmCCEhMaiorQueValorEmCp = valorEmCC > valorEmCP;
+        System.out.println("CC > CP ? " + valorEmCCEhMaiorQueValorEmCp);
+
+        boolean valorEmCCEhMenorQueValorEmCp = valorEmCC < valorEmCP;
+        System.out.println("CC < CP ? " + valorEmCCEhMenorQueValorEmCp);
+
+        // Operadores lógicos && e || ou
+        boolean temDinheiro = valorEmCC > valorPS5 || valorEmCP > valorPS5 || (valorEmCC + valorEmCP > valorPS5);
+        System.out.println("Tem dinheiro pra comprar PS5? " + temDinheiro);
+
+        boolean temDinheiroOuCredito = valorEmCC > valorPS5 || valorEmCP > valorPS5 || valorEmCC + valorEmCP > valorPS5
+                                                            || (valorEmCP + valorEmCC < valorPS5 && credito);
+        System.out.println("Tem dinheiro ou crédito? " + temDinheiroOuCredito);
+    }
+}
