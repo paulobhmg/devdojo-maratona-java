73781643f2cb7d91dd6604e096f2b7cc998ab69c
Paulo Nogueira <paulobh.nogueira@gmail.com>
2022-03-17 19:06:54 -0700

    Entendendo as lógicas de break, continue e suas diferenças.

---
+++ src/com/paulo/m01_basico/A030_BreakOuContinueNoLacoDeRepeticao.java
@@ -0,0 +1,27 @@
+package com.paulo.m01_basico;
+
+public class A030_BreakOuContinueNoLacoDeRepeticao {
+    public static void main(String[] args) {
+        double parcela = 0;
+        double valorDoCarro = 28000;
+
+        // Interrompendo laço de repetição com BREAK
+        for(int i = 1; i <= valorDoCarro; i++) {
+            parcela = valorDoCarro / i;
+            if(parcela < 1000) {
+                break;
+            }
+            System.out.println(i + " x R$ " + parcela);
+        }
+        System.out.println("------------------------");
+
+        // Alterando a lógica do cálculo com o CONTINUE
+        for(int i = (int) valorDoCarro; i >= 1; i--){
+            parcela = valorDoCarro / i;
+            if(parcela < 1000){
+                continue;
+            }
+            System.out.println(i + " x R$ " + parcela);
+        }
+    }
+}
