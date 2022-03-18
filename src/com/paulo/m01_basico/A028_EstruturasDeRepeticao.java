4c32f2c1e4e73256cb358a8dbd0ba718ab236e40
Paulo Nogueira <paulobh.nogueira@gmail.com>
2022-03-17 18:08:41 -0700

    Conhecendo as estruturas while, do while e for.

---
+++ src/com/paulo/m01_basico/A028_EstruturasDeRepeticao.java
@@ -0,0 +1,47 @@
+package com.paulo.m01_basico;
+
+public class A028_EstruturasDeRepeticao {
+    public static void main(String[] args) {
+        int i = 0;
+
+        // while só é executado caso a expressão no parâmetro seja verdadeira
+        while(i <= 10) {
+            System.out.print(i);
+            if(i < 10)
+                System.out.print(", ");
+            i++;
+        }
+        System.out.println();
+
+        // do-while só faz a verificação após o código ser executado ao menos uma vez.
+        i = 0;
+        do {
+            System.out.print(i);
+            if(i < 10) {
+                System.out.print(", ");
+            }
+            i++;
+        } while(i <= 10);
+        System.out.println();
+
+        // for irá executar o código X vezes, com base no parâmetro passado na assinatura do laço
+        for(i = 0; i <= 10; i++){
+            System.out.print(i);
+            if(i < 10){
+                System.out.print(", ");
+            }
+        }
+        System.out.println();
+
+        // Imprimindo apenas números pares dentro de um intervalo com for
+        for(i = 0; i <= 100; i++){
+            if(i % 2 == 0){
+                System.out.print(i);
+                if(i < 100){
+                    System.out.print(", ");
+                }
+            }
+        }
+        System.out.println();
+    }
+}
