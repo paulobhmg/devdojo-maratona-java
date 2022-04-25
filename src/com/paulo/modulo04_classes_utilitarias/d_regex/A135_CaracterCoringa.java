a4918d09ad70a5073b8c2e6e31ce26e0f291213e
Paulo Nogueira <paulobh.nogueira@gmail.com>
2022-04-25 13:30:57 -0700

    Exercício prático regex - Valida e-mail.

---
+++ src/com/paulo/modulo04_classes_utilitarias/d_regex/A135_CaracterCoringa.java
@@ -0,0 +1,34 @@
+package com.paulo.modulo04_classes_utilitarias.d_regex;
+
+import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class A135_CaracterCoringa {
+    static Scanner entrada = new Scanner(System.in);
+    public static void main(String[] args) {
+        /* Além dos outros quantificadores, tem também o quantificador coringa ".", que é utilizado para recuperar
+           valores entre dois intervalos. Por exemplo, em um texto paulo12345, um regex 1.5 irá retornar tudo que está
+           entre 1 e 3, dentro da expressão.
+
+         ** Verificar sites para testar regex online. */
+        String texto = "paulo12345";
+        Pattern regex = Pattern.compile("(1.3)+"); // Testar isso depois
+        Matcher matcher = regex.matcher(texto);
+
+        while(matcher.find()){
+            System.out.printf("%s - %s\n", matcher.start(), matcher.group());
+        }
+        System.out.println("---------------");
+
+        //Validando e-mails válidos.
+        String email = entrada.nextLine();
+        regex = Pattern.compile("([a-zA-Z]+[a-zA-Z0-9\\._-]*)@[a-zA-Z]+(\\.([a-zA-Z]+)){1,2}$");
+        matcher = regex.matcher(email);
+
+        System.out.println(email.matches(regex.toString()));
+        while(matcher.find()){
+            System.out.printf("%s", matcher.group());
+        }
+    }
+}
